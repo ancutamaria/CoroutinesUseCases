2b46fce896d6669aa975016b595d3b72dbdd6de0
ancuta <ancuta.maria@gmail.com>
2021-04-17 10:37:18 +0300

    Sequential Network Requests - Coroutines style

--- app/src/main/java/com/am/coroutinesusecases/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
+++ app/src/main/java/com/am/coroutinesusecases/usecases/coroutines/usecase2/Perform2SequentialNetworkRequestsViewModel.kt
@@ -1,13 +1,30 @@
 package com.am.coroutinesusecases.usecases.coroutines.usecase2
 
+import androidx.lifecycle.viewModelScope
 import com.am.coroutinesusecases.base.BaseViewModel
 import com.am.coroutinesusecases.mock.MockApi
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class Perform2SequentialNetworkRequestsViewModel(
     private val mockApi: MockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
     fun perform2SequentialNetworkRequest() {
+        uiState.value = UiState.Loading
 
+        viewModelScope.launch {
+            try {
+                val recentVersions = mockApi.getRecentAndroidVersions()
+                val mostRecentVersion = recentVersions.last()
+                val featuresOfMostRecentVersion =
+                    mockApi.getAndroidVersionFeatures(mostRecentVersion.apiLevel)
+                uiState.value = UiState.Success(featuresOfMostRecentVersion)
+            } catch (exception: Exception) {
+                uiState.value = UiState.Error("Smth went wrong")
+            }
+        }
     }
+
+    //the coroutine get cancelled automatically so we don't need to worry about memory leaks
 }
