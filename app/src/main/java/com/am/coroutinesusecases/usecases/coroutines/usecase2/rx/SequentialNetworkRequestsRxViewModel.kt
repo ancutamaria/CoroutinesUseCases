81766c3ec338af310836964fcd36fe721beea1b6
ancuta <ancuta.maria@gmail.com>
2021-04-17 10:23:10 +0300

    Sequential Network Requests - RxJava style

--- app/src/main/java/com/am/coroutinesusecases/usecases/coroutines/usecase2/rx/SequentialNetworkRequestsRxViewModel.kt
+++ app/src/main/java/com/am/coroutinesusecases/usecases/coroutines/usecase2/rx/SequentialNetworkRequestsRxViewModel.kt
@@ -1,12 +1,41 @@
 package com.am.coroutinesusecases.usecases.coroutines.usecase2.rx
 
 import com.am.coroutinesusecases.base.BaseViewModel
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.schedulers.Schedulers
 
 class SequentialNetworkRequestsRxViewModel(
     private val mockApi: RxMockApi = mockApi()
 ) : BaseViewModel<UiState>() {
 
+    private val disposables = CompositeDisposable()
     fun perform2SequentialNetworkRequest() {
+        uiState.value = UiState.Loading
 
+        mockApi.getRecentAndroidVersions()
+            .flatMap { androidVersions ->
+                val recentVersion = androidVersions.last()
+                mockApi.getAndroidVersionFeatures(recentVersion.apiLevel)
+            }
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribeBy (
+                onSuccess = { featuresVersion ->
+                    uiState.value = UiState.Success(featuresVersion)
+                },
+                onError = {
+                    uiState.value = UiState.Error("Smth went wrong")
+                }
+                    )
+            .addTo(disposables)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        //to avoid memory leaks
+        disposables.clear()
     }
 }
