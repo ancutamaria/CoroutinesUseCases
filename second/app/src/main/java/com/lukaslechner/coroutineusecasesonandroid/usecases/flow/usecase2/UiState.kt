b565df88774832df33d626b14976c5cd812577ad
ancutamaria <ancuta.maria@gmail.com>
2024-06-26 11:46:15 +0300

    adjust folders

--- second/app/src/main/java/com/lukaslechner/coroutineusecasesonandroid/usecases/flow/usecase2/UiState.kt
+++
@@ -1,9 +0,0 @@
-package com.lukaslechner.coroutineusecasesonandroid.usecases.flow.usecase2
-
-import com.lukaslechner.coroutineusecasesonandroid.usecases.flow.mock.Stock
-
-sealed class UiState {
-    object Loading : UiState()
-    data class Success(val stockList: List<Stock>) : UiState()
-    data class Error(val message: String) : UiState()
-}
